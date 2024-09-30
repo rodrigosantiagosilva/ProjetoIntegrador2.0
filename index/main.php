5b9dc97bda59082bc4857f80ea5da449a9929250
Your Name <you@example.com>
2024-09-30 13:36:29 -0300

    coiso

--- index/main.php
+++
@@ -1,6 +0,0 @@
-    <main class="first">
-        <div class="comercial"></div>
-        <div class="tecnologia"></div>
-        <div class="militar"></div>
-        <div class="Linha do tempo"></div>
-    </main>
